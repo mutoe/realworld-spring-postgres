d9e934ca292a8f89cf40efcd7b2168c43cadd61c
mutoe <mutoe@foxmail.com>
2019-11-19 16:28:11 +0800

    chore: initial

---
+++ src/main/java/com/mutoe/realworld/spring_postgres/SpringPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.mutoe.realworld.spring_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgresApplication.class, args);
+    }
+
+}
